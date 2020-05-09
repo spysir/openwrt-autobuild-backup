de631f173d9d5b9cf9f83f6936eaa5c1b0f68dc4
weweyes <59646715+weweyes@users.noreply.github.com>
2020-05-09 10:14:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,4 @@ sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generat
 git clone https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash
 git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
 git clone https://github.com/weweyes/luci-app-eqos.git package/lean/luci-app-eqos
+git clone https://github.com/destan19/OpenAppFilter.git package/lean/luci-app-oaf
