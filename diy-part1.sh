478d6ef49439d21b32fb2232b8312af82fb84faf
weweyes <59646715+weweyes@users.noreply.github.com>
2020-05-17 10:26:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,13 +8,13 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# Uncomment a feed source
+# Uncomment a feed source 
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/weweyes/openwrt-package' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/weweyes/openwrt-packages' feeds.conf.default
 # Add passwall
-sed -i '$a src-git passwall https://github.com/weweyes/luci-app-passwall' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/weweyes/luci-app-passwall' feeds.conf.default
 # Add passwall dependency package
-sed -i '$a src-git small https://github.com/weweyes/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/weweyes/small' feeds.conf.default
