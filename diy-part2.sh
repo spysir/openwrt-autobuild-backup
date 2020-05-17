22cf103f3852c76928f05641853d865a237ff562
weweyes <59646715+weweyes@users.noreply.github.com>
2020-05-17 10:24:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,4 +16,4 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-ap
 git clone https://github.com/weweyes/luci-app-eqos.git package/lean/luci-app-eqos
 git clone https://github.com/destan19/OpenAppFilter.git package/lean/luci-app-oaf
 git clone https://github.com/weweyes/luci-app-passwall.git package/lean/luci-app-passwall
-git clone https://github.com/weweyes/small.git package/
+git clone https://github.com/weweyes/small.git package/small
