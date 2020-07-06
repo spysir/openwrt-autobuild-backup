a76e73fd236a200ca04573eb796306238396b39f
weweyes <59646715+weweyes@users.noreply.github.com>
2020-07-06 09:56:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
 # add app
-git clone https://github.com/weweyes/OpenClash.git package/lean/luci-app-openclash
-git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
-git clone https://github.com/littletao08/luci-app-eqos.git package/lean/luci-app-eqos
-git clone https://github.com/destan19/OpenAppFilter.git package/lean/luci-app-oaf
+git clone https://github.com/weweyes/OpenClash.git package/luci-app-openclash
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/littletao08/luci-app-eqos.git package/luci-app-eqos
+git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 
 #git clone https://github.com/weweyes/luci-app-passwall.git package/lean/luci-app-passwall
 #git clone https://github.com/weweyes/small.git package/lean/small
