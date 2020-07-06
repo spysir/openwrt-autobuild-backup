c795d848ace1c7ec2457c78ccb82d73750e1d9bc
weweyes <59646715+weweyes@users.noreply.github.com>
2020-07-06 09:50:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,8 +15,9 @@ git clone https://github.com/weweyes/OpenClash.git package/lean/luci-app-opencla
 git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
 git clone https://github.com/littletao08/luci-app-eqos.git package/lean/luci-app-eqos
 git clone https://github.com/destan19/OpenAppFilter.git package/lean/luci-app-oaf
-git clone https://github.com/weweyes/luci-app-passwall.git package/lean/luci-app-passwall
-git clone https://github.com/weweyes/small.git package/lean/small
+
+#git clone https://github.com/weweyes/luci-app-passwall.git package/lean/luci-app-passwall
+#git clone https://github.com/weweyes/small.git package/lean/small
 
 #svn co https://github.com/weweyes/openwrt-packages/trunk/luci-theme-opentomato package/lean/luci-theme-opentomato
 #svn co https://github.com/weweyes/openwrt-packages/trunk/luci-theme-opentomcat package/lean/luci-theme-opentomcat
