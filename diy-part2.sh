6bfd18577bbeabd42bb9842e5884d85cca2aa2e9
weweyes <59646715+weweyes@users.noreply.github.com>
2020-09-14 18:04:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,14 +10,24 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
-# add app
+
+# Add app
 git clone https://github.com/weweyes/OpenClash.git package/luci-app-openclash
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/weweyes/luci-app-eqos.git package/luci-app-eqos
 git clone https://github.com/weweyes/OpenAppFilter.git package/luci-app-oaf
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/luci-app-smartdns
+git clone https://github.com/weweyes/luci-app-tcpdump.git package/luci-app-tcpdump
 
-# add theme
+git clone https://github.com/jerrykuku/node-request.git package/node-request
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+
+svn co https://github.com/weweyes/openwrt-packages/trunk/luci-app-smartdns package/luci-app-smartdns
+svn co https://github.com/weweyes/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+svn co https://github.com/weweyes/openwrt-packages/trunk/luci-app-aliddns package/luci-app-aliddns
+
+# Add theme
 svn co https://github.com/weweyes/openwrt-packages/trunk/luci-theme-opentomcat package/luci-theme-opentomcat
-#git clone https://github.com/weweyes/luci-app-argon-config.git package/luci-app-argon-config
-#git clone -b 18.06 https://github.com/weweyes/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/weweyes/luci-app-argon-config.git package/luci-app-argon-config
+git clone -b 18.06 https://github.com/weweyes/luci-theme-argon.git package/luci-theme-argon
