a29a030511ad0ffd91eef4dd973d8604584a4a09
weweyes <59646715+weweyes@users.noreply.github.com>
2020-12-07 16:20:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Modify default IP (admin IP=192.168.8.5 user=root pw=password)
 sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
 # Change default theme to luci-thme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-opentopd/g' ./feeds/luci/collections/luci/Makefile
 
