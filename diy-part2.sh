a53b1dd2c3ee2f40d0b54c500c00f83cc897a985
weweyes <59646715+weweyes@users.noreply.github.com>
2020-12-29 16:38:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 # Modify default IP (admin IP=192.168.8.5 user=root pw=password)
 sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generate
-# Change default theme to luci-thme-argon
+# Change default theme to luci-theme-opentopd
 sed -i 's/luci-theme-bootstrap/luci-theme-opentopd/g' ./feeds/luci/collections/luci/Makefile
 # 版本号里显示一个自己的名字和编译日期（XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
