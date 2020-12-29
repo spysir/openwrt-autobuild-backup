0846e5387e1d3c8f74a4a170ca35b59b4771b2bf
weweyes <59646715+weweyes@users.noreply.github.com>
2020-12-29 20:27:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,5 @@ sed -i 's/192.168.1.1/192.168.8.5/g' package/base-files/files/bin/config_generat
 sed -i 's/luci-theme-bootstrap/luci-theme-opentopd/g' ./feeds/luci/collections/luci/Makefile
 # 版本号里显示一个自己的名字和编译日期（XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /XPK build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+# Set the password to be empty
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
