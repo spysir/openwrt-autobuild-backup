3c9f8f2098ed0a684433935122e9e91ec7e10297
weweyes <59646715+weweyes@users.noreply.github.com>
2020-12-29 19:07:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -35,4 +35,4 @@ svn co https://github.com/weweyes/openwrt-package/trunk/luci-app-aliddns package
 git clone https://github.com/weweyes/luci-app-argon-config.git package/luci-app-argon-config
 git clone -b 18.06 https://github.com/weweyes/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/weweyes/openwrt-themes.git package/openwrt-themes
-git clone https://github.com/weweyes/luci-theme-opentopd.git package/luci-theme-opentopd
+#git clone https://github.com/weweyes/luci-theme-opentopd.git package/luci-theme-opentopd
